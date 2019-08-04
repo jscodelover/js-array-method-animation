ed1c28f453442df8f126cfb53fe809c6543d400c
jscodelover <manisha.4918@gmail.com>
2019-08-04 19:47:12 +0530

    feat: button next and previous

--- src/container/Home/home.style.js
+++ src/container/Home/home.style.js
@@ -1,7 +1,40 @@
 import style from 'styled-components';
+import theme from 'styled-theming';
+import defaultTheme from '../../style/themes';
+
+const svgFill = theme('mode', {
+	light: defaultTheme.colors.black,
+	dark: defaultTheme.colors.white
+});
 
 const HomeStyle = style.div`
-    margin-top: 50px;
+height: 100vh;
+    margin: 70px 10%;
+    .btn-container{
+        display: flex;
+        justify-content: space-evenly;
+        .btn-previous, .btn-next{
+            display: flex;
+            align-items: center;
+            svg{
+                fill: ${svgFill};
+            }
+        }
+        .btn-next{
+            svg{
+                margin-left: 10px;
+            }
+        }
+        .btn-previous{
+            svg{
+                margin-right: 10px;
+                transform: rotate(180deg);
+            }
+        }
+    }
+    @media (max-width: 628px){
+        margin: 70px 5%;
+    }
 `;
 
 export { HomeStyle };
--- src/container/Home/index.jsx
+++ src/container/Home/index.jsx
@@ -3,12 +3,26 @@ import { HomeStyle } from './home.style';
 import Button from '../../components/button';
 
 function Home(){
+    const svg = (
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"/></svg>
+    )
     return(
         <HomeStyle>
-            <Button type="btn">Previous</Button>
-            <Button type="btn">Next</Button>
-        />
-        </HomeStyle>
+            <div className="btn-container">
+                <div className="btn-previous"> 
+                    {svg}  
+                    <Button type="btn">
+                        Previous
+                    </Button>
+                </div>
+                <div className="btn-next">
+                    <Button type="btn"> 
+                        Next
+                    </Button>
+                    {svg}
+                </div>
+            </div>
+       </HomeStyle>
     );
 }
 

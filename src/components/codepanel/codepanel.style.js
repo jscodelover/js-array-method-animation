13417d836154d947d7680ca783788b18016914f3
jscodelover <manisha.4918@gmail.com>
2019-08-06 10:45:37 +0530

    refactor(codepanel): bgcolor and font

--- src/components/codepanel/codepanel.style.js
+++ src/components/codepanel/codepanel.style.js
@@ -8,25 +8,26 @@ const textColor = theme('mode', {
 });
 
 const bgColor = theme('mode', {
-	light: defaultTheme.colors.black,
-	dark: defaultTheme.colors.white
+	light: 'linear-gradient(45deg, rgba(41, 39, 39, 0.88) , rgb(41, 38, 38))',
+	dark:
+		'linear-gradient(45deg, rgb(226, 223, 223), rgba(255,255,255,1), rgb(214, 207, 207))'
 });
 
 const borderShadow = theme('mode', {
 	light: 'rgba(0,0,0,.5)',
-	dark: 'rgba(255,255,255,.3)'
+	dark: 'rgba(115, 115, 115, 0.63)'
 });
 
 const CodePanelStyle = style.div`
-    background-color: ${bgColor};
+    background-image: ${bgColor};
     color: ${textColor};
     margin: 10px 50px;
     padding: 20px;
     border-radius: 10px;
     box-shadow: 2px 2px 13px 1px ${borderShadow};
+    font-family: 'Fira Code', monospace;
     & > div{
         line-height: 22px;
-        letter-spacing: 0.03em;
     }
     @media (max-width: 628px){
         padding: 10px;
--- src/components/codepanel/index.jsx
+++ src/components/codepanel/index.jsx
@@ -5,11 +5,7 @@ function CodePanel(props){
     const { children } = props;
     return (
         <CodePanelStyle>
-            {/* { children } */}
-            <div>var array1 = [1, 4, 9, 16];</div>
-<div>const map1 = array1.map(x => x * 2);</div>
-
-<div>console.log(map1);</div>
+            { children }
         </CodePanelStyle>
     )
 }

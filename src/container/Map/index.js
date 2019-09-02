da4e8e4697d0c8c25f024a14e35e59f8c3987cdd
jscodelover <manisha.4918@gmail.com>
2019-09-02 11:50:56 +0530

    refactor: update animation effect map

--- src/container/Map/index.js
+++ src/container/Map/index.js
@@ -18,9 +18,10 @@ function Map() {
 	const block9 = useRef(null);
 	const block10 = useRef(null);
 	useEffect(() => {
-		TweenMax.to(mapFn.current, 4, {
+		TweenMax.to(mapFn.current, 2, {
 			rotation: 360,
-			ease: Power0.easeOut
+			ease: Power0.easeOut,
+			repeat: 1
 		}).delay(3.6);
 		TweenMax.fromTo(
 			block1.current,

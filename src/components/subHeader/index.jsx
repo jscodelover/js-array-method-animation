b0d473a1f5d225e6bda993e3656ced1fa8da567b
jscodelover <manisha.4918@gmail.com>
2019-11-06 13:13:55 +0530

    fix: previous and next button ticket5

--- src/components/subHeader/index.jsx
+++ src/components/subHeader/index.jsx
@@ -6,30 +6,23 @@ import Button from '../button';
 function SubHeader(props){
     const routes = ['/filter', '/map', '/find', '/findindex', '/reduce', '/sort'];
     const [ route, handleRoute ] = useState(routes.findIndex(item => item === props.location.pathname));
-    // useEffect(()=>{
-    //     console.log(route);
-    //     props.history.push(routes[route]);
-    // },[route]);
-    console.log(props.location.pathname)
-    async function handleRouteUpdate(pos){
-        await handleRoute(pos);
+    useEffect(()=>{
         props.history.push(routes[route]);
-    }
+    },[route]);
     const svg = (
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"/></svg>
     )
-    console.log(route);
     return(
         <SubHeaderStyle>
             <div className="btn-container">
                 <div className="btn-previous"> 
                     {svg}  
-                    <Button type="btn" fn={() => handleRouteUpdate(route < 1 ? 0 : route - 1)}>
+                    <Button type="btn" fn={() => handleRoute(route < 1 ? 0 : route - 1)}>
                         Previous
                     </Button>
                 </div>
                 <div className="btn-next">
-                    <Button type="btn" fn={() => handleRouteUpdate(route === routes.length-1 ? routes.length-1 : route + 1)}> 
+                    <Button type="btn" fn={() => handleRoute(route === routes.length-1 ? routes.length-1 : route + 1)}> 
                         Next
                     </Button>
                     {svg}

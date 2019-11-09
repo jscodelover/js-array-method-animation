6413bd46f036e42fec36aa7569d582cd2049470f
jscodelover <manisha.4918@gmail.com>
2019-11-09 19:53:48 +0530

    feat: implementation for reduce, map, filter, find. findIndex, some, every, from

--- src/utils/custom_method.js
+++ src/utils/custom_method.js
@@ -1,8 +1,55 @@
 export const custom_method = {
-	myMap: `<p>Array.prototype.myMap = function(fn){</p> 
-                <p>for(let i=0; i < this.length; i++ ){</p>
-                    <p>fn(this[i]);</p>
+	myMap: `<p>Array.prototype.myMap = function(fn) {</p>
+                <p>const result = [];</p>
+                <p>for (let i = 0; i < this.length; i++) {</p>
+                    <p>result.push(fn(this[i], i));</p>
                 <p>}</p>
-            <p>}</p>
-            <p>[1,2,3].myMap((i) => console.log(i*2));</p>`
+                <p> return result;</p>
+            <p>}</p>;`,
+
+	myFilter: `<p>Array.prototype.myFilter = function(fn) {</p>
+                <p>const result = [];</p>
+                <p>for (let i = 0; i < this.length; i++) {</p>
+                    <p>if (fn(this[i], i)) result.push(this[i]);</p>
+                <p>}</p>
+                <p>return result;</p>
+            <p>};</p>`,
+
+	myFind: `<p>Array.prototype.myFind = function(fn) {</p>
+                <p>for (let i = 0; i < this.length; i++) {</p>
+                    <p>if (fn(this[i], i)) return this[i];</p>
+                <p>}</p>
+                <p>return undefined;</p>
+            <p>};</p>`,
+	myFindIndex: `<p>Array.prototype.myFindIndex = function(fn) {</p>
+                    <p>for (let i = 0; i < this.length; i++) {</p>
+                        <p>if (fn(this[i], i)) return i;</p>
+                    <p>}</p>
+                    <p>return -1;</p>
+                <p>};</p>`,
+	myReduce: `<p>Array.prototype.myReduce = function(fn, arg) {</p>
+                    <p>let result = arg;</p>
+                    <p>for (let i = 0; i < this.length; i++) {</p>
+                        <p>result = fn(result, this[i], i);</p>   
+                    <p>}</p>
+                    <p>return result;</p>
+                <p>};</p>`,
+	mySome: `<p>Array.prototype.mySome = function(fn) {</p>
+                <p>for (let i = 0; i < this.length; i++) {</p>
+                    <p>if (fn(this[i], i)) return true;</p>
+                <p>}</p>
+                <p>return false;</p>
+            <p>};</p>`,
+	myEvery: `<p>Array.prototype.myEvery = function(fn) {</p>
+                    <p>for (let i = 0; i < this.length; i++) {</p>
+                        <p>if (!fn(this[i], i)) return false;</p>
+                    <p>}</p>
+                    <p>return true;</p>
+                <p>};</p>`,
+	myFrom: `<p>Array.myFrom = function(data){</p>
+                <p>const result = [];</p>
+                <p>for(let i = 0; i < data.length; i++)</p>
+                    <p>result.push(data[i]);</p>
+                <p>return result;</p>    
+            <p>};</p>`
 };

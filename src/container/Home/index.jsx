4f6a84cb48a180c0f79c52f52629a81087dd3bba
jscodelover <manisha.4918@gmail.com>
2019-11-14 16:27:26 +0530

    refactor: nav

--- src/container/Home/index.jsx
+++ src/container/Home/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { HomeStyle } from './home.style';
 
-function Home(){
-    return (
-        <HomeStyle>
-            <p>
-                We often commit complex ideas to memory by visualizing them. There aren’t many coding articles using visuals as starting point for learning. And yet, visualization plays important role in education. This also applies to coding.
-            </p>
-        </HomeStyle>
-    )
+function Home() {
+	return (
+		<HomeStyle>
+			<p>
+				We often commit complex ideas to memory by visualizing them. There
+				aren’t many coding articles using visuals as starting point for
+				learning. And yet, visualization plays important role in education. This
+				also applies to coding.
+			</p>
+		</HomeStyle>
+	);
 }
 
 export default Home;
